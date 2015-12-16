45e8f3a39c28bf4ddc7134003a5a125f7f23709e
Adam Crews <adamc@slalom.com>
2015-12-15 16:22:13 -0800

    WIP: Windows testing

--- manifests/install.pp
+++ manifests/install.pp
@@ -95,7 +95,7 @@ class splunk::install (
       purge              => false,
       source_permissions => ignore,
       source             => 'puppet:///modules/splunk/noarch/opt/splunk/etc/auth',
-      ignore             => [ '*/splunkweb' ],
+      ignore             => [ '*/splunkweb', 'C:/Program Files/SplunkUniversalForwarder/etc/auth/splunkweb/cert.pem' ],
       require            => Package[$pkgname],
       before             => Service[$service_name],
   }
